4051d855dbd51f4d095efd12279dbc2aeaa10d7c
Samuel Mwalami <samuelmwalami@gmail.com>
2023-02-13 19:09:19 +0300

    made changes to some files

--- templates.py
+++ templates.py
@@ -6,6 +6,9 @@ app = Flask(__name__ ,template_folder='template')
 def home():
     return render_template("index.html")
 
+@app.route('/decision')
+def decisions():
+    return render_template("decision.html",content="John")
 
 if __name__ == "__main__":
     app.run(debug = True)
